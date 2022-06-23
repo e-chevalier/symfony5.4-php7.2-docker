b17b86990378a49b5c9687858577773ca9e95c55
echevalier <estebanchevalier@gmail.com>
2022-06-23 10:52:13 -0300

    Add publishen and check outline message

--- src/Controller/PublisherController.php
+++ src/Controller/PublisherController.php
@@ -11,21 +11,25 @@ use Symfony\Component\Mercure\Update;
 
 class PublisherController extends AbstractController
 {
+
+     /**
+     * @Route("/publish", name="app_publish")
+     */
+    public function publish(): Response
+    {
+        return $this->render('publisher/index.html.twig', [
+            'controller_name' => 'PublisherController',
+        ]);
+    }
+
     /**
      * @Route("/publisher/{topic}", name="app_publisher", methods={"POST"})
      */
     public function index(HubInterface $hub, $topic, Request $request): Response
     {
-        
-        dump($topic);
-        dump(json_encode($request->getContent()));
-        //dump($request->getContent());
+        $parameters = $request->request->all();
 
-        $update = new Update( $topic, $request->getContent());
-        //$update = new Update( $topic, json_encode($request->getContent()));
-
-        //$update = new Update( $topic, json_encode(['status' => 'OutOfStock']), false);
-
+        $update = new Update( $topic, json_encode($parameters));
         $hub->publish($update);
 
         return new Response('published!');
